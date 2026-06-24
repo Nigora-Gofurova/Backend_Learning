e17eb7a0ef2d2110ec2dd6b76aa87c0385417874
GOFUROVA NIGORAXON <nigiwgafurova@gmail.com>
2026-06-24 15:36:06 +0500

    hato tuzatildi

---
+++ src/Homework/Task_2.java
@@ -0,0 +1,27 @@
+package Homework;
+
+import java.util.Scanner;
+
+public class Task_2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Bo'yingizni kiriting (metrda, masalan 1.75): ");
+        double boy = input.nextDouble();
+
+        System.out.print("Vazningizni kiriting (kg): ");
+        double vazn = input.nextDouble();
+
+        double bmi = vazn / (boy * boy);
+        
+        if (bmi < 18.5) {
+            System.out.print("Ozginsiz");
+        } else if (bmi >= 18.5 && bmi < 25) {
+            System.out.print("Normal holatdasiz");
+        } else if (bmi >= 25 && bmi < 30) {
+            System.out.print("Ortiqcha vazningiz bor");
+        } else {
+            System.out.print("Semizlik");
+        }
+    }
+
+}
