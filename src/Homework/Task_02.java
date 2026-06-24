1a0baff8f6723df41167c74ff7ddb7f5609e6343
GOFUROVA NIGORAXON <nigiwgafurova@gmail.com>
2026-06-24 16:42:36 +0500

    Update Task_02.java
    fixsed class name

--- src/Homework/Task_02.java
+++ src/Homework/Task_02.java
@@ -2,17 +2,17 @@ package Homework;
 
 import java.util.Scanner;
 
-public class Task_2 {
+public class  Task_02 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.print("Bo'yingizni kiriting (metrda, masalan 1.75): ");
+        System.out.print("Bo'yingizni kiriting :");
         double boy = input.nextDouble();
 
-        System.out.print("Vazningizni kiriting (kg): ");
+        System.out.print("Vazningizni kiriting (kg) :");
         double vazn = input.nextDouble();
 
         double bmi = vazn / (boy * boy);
-        
+
         if (bmi < 18.5) {
             System.out.print("Ozginsiz");
         } else if (bmi >= 18.5 && bmi < 25) {
